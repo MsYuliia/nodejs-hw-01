446515f36e8cfb4ec1bf98aa5b0967765237bf59
Yuliia Romanovych <jdani22101998@gmail.com>
2025-03-26 23:40:06 +0200

    done hw-1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,18 @@
-const generateContacts = async (number) => {};
+
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+
+    const newContacts = Array.from({ length: number }, createFakeContact);
+
+    await writeContacts( [...contacts, ...newContacts]);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts);
+
+    await fs.writeFile(PATH_DB, data);
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+};
